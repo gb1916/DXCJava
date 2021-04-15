d5267d0a39eee845440211fbf4feeda5e49f5d24
gb1916 <guntupallibabitha@gmail.com>
2021-04-15 17:40:55 +0530

    programs

---
+++ Synchronization.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+class Synchronization
+{
+   public static void main (String [] args)
+   {
+      FTrans ft = new FTrans ();
+      TransThread tt1 = new TransThread (ft, "Deposit Thread");
+      TransThread tt2 = new TransThread (ft, "Withdrawal Thread");
+      tt1.start ();
+      tt2.start ();
+   }
+}
+class FTrans
+{
+   public static String FTName;
+   public static double amount;
+}
+class TransThread extends Thread
+{
+   private FTrans ft;
+   TransThread (FTrans ft, String name)
+   {
+      super (name); // Save thread's name
+      this.ft = ft; // Save reference to financial transaction object
+   }
+   public void run ()
+   {
+      for (int i = 0; i < 100; i++)
+      {
+           if (getName ().equals ("Deposit Thread"))
+           {
+               // Start of deposit thread's critical code section
+               ft.FTName = "Deposit";
+               try
+               {
+                  Thread.sleep ((int) (Math.random () * 1000));
+               }
+               catch (InterruptedException e)
+               {
+               }
+               ft.amount = 2000.0;
+               System.out.println (ft.FTName + " " + ft.amount);
+               // End of deposit thread's critical code section
+           }
+           else
+           {
+               // Start of withdrawal thread's critical code section
+               ft.FTName = "Withdrawal";
+               try
+               {
+                  Thread.sleep ((int) (Math.random () * 1000));
+               }
+               catch (InterruptedException e)
+               {
+               }
+               ft.amount = 250.0;
+               System.out.println (ft.FTName + " " + ft.amount);
+               // End of withdrawal thread's critical code section
+           }
+      }
+   }
+}
