7a7fb1ad9d9170d5f8da0be455661766614ab7c6
gb1916 <guntupallibabitha@gmail.com>
2021-04-29 10:47:45 +0530

    ..

--- ex-3/ReadStringTest.java
+++
@@ -1,9 +0,0 @@
-import java.io.Console;  
-class ReadStringTest{    
-public static void main(String args[]){    
-Console c=System.console();    
-System.out.println("What is your Name: ");    
-String n=c.readLine();    
-System.out.println("Hello " +n);    
-}    
-}  
